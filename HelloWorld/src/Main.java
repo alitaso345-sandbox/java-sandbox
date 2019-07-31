f69d2792097bd5043434f054be08f1b4df4f1271
alitaso345 <alice.maru345@gmail.com>
2019-07-31 23:16:15 +0900

    if

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,8 +1,14 @@
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
-        int numberA;
-        numberA = 10;
-        System.out.println(numberA);
+        int second = LocalDateTime.now().getSecond();
+
+        if (second % 2 == 0) {
+            System.out.println(second + "は偶数です");
+        } else {
+            System.out.println(second + "は奇数です");
+        }
     }
 }
