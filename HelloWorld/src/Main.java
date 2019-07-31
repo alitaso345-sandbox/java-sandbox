97f596f71696ce038a0ce6c34a696a44a769dabf
alitaso345 <alice.maru345@gmail.com>
2019-07-31 23:23:06 +0900

    case

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,14 +1,31 @@
 import java.time.LocalDateTime;
+import java.time.Month;
 
 public class Main {
 
     public static void main(String[] args) {
-        int second = LocalDateTime.now().getSecond();
+        Month month = LocalDateTime.now().getMonth();
 
-        if (second % 2 == 0) {
-            System.out.println(second + "は偶数です");
-        } else {
-            System.out.println(second + "は奇数です");
+        switch (month) {
+            case MARCH:
+            case APRIL:
+            case MAY:
+                System.out.println(month + "は春です");
+                break;
+            case JUNE:
+            case JULY:
+            case AUGUST:
+                System.out.println(month + "は夏です");
+                break;
+            case SEPTEMBER:
+            case OCTOBER:
+            case NOVEMBER:
+                System.out.println(month + "は秋です");
+                break;
+            default:
+                System.out.println(month + "は冬です");
+                break;
         }
+
     }
 }
