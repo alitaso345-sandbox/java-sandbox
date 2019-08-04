432a2fe203d109a6c7ea60d844b3e362d8b35f14
alitaso345 <alice.maru345@gmail.com>
2019-08-04 17:07:24 +0900

    iterator

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,10 +1,26 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 2, 3, 5, 8, 13};
-        System.out.println(array);
-        System.out.println(Arrays.toString(array));
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Ken", 100));
+        students.add(new Student("Shin", 60));
+        students.add(new Student("Takuya", 80));
+
+        Iterator<Student> iterator = students.iterator();
+        while(iterator.hasNext()) {
+            Student student = iterator.next();
+            if (student.getScore() < 70) {
+                iterator.remove();
+            }
+        }
+
+        for (Student student : students) {
+            System.out.println(student.getName() + ":" + student.getScore());
+        }
     }
 }
--- HelloWorld/src/StudentSample.java
+++ HelloWorld/src/StudentSample.java
@@ -1,11 +1,24 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class StudentSample {
     public static void main(String... args) {
-        Student murata = new Student("村田");
+        Student[] students = {
+                new Student("Ken", 100),
+                new Student("Shin", 60),
+                new Student("Takuya", 80)
+        };
 
-        murata.score = 80;
-        murata.printScore();
+        Comparator<Student> comparator = new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return Integer.compare(o2.getScore(), o1.getScore());
+            }
+        };
 
-        Student okada = new Student("岡田", 90);
-        okada.printScore();
+        Arrays.sort(students, comparator);
+        for (Student student : students) {
+            System.out.println(student.getName() + ":" + student.getScore());
+        }
     }
 }
