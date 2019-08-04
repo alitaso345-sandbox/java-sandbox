0b026b34d11eae470fea88bc83bfa99626879ea1
alitaso345 <alice.maru345@gmail.com>
2019-08-04 16:42:08 +0900

    Arrays.toString

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,16 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        NumberStack<Integer> intStack = new NumberStack<>();
-        NumberStack<Long> longStack = new NumberStack<>();
-
-        intStack.push(100);
-        intStack.push(200);
-
-        Integer numElement = intStack.pop();
-
-        if (numElement != null) {
-            System.out.println(numElement);
-        }
+        int[] array = {1, 2, 2, 3, 5, 8, 13};
+        System.out.println(array);
+        System.out.println(Arrays.toString(array));
     }
 }
