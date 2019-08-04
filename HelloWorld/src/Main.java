67ca12914a59117198b4ddc15c6cb7475ce44864
alitaso345 <alice.maru345@gmail.com>
2019-08-04 19:52:06 +0900

    create stream from list

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,26 +1,12 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Ken", 100));
-        students.add(new Student("Shin", 60));
-        students.add(new Student("Takuya", 80));
-
-        Iterator<Student> iterator = students.iterator();
-        while(iterator.hasNext()) {
-            Student student = iterator.next();
-            if (student.getScore() < 70) {
-                iterator.remove();
-            }
-        }
-
-        for (Student student : students) {
-            System.out.println(student.getName() + ":" + student.getScore());
-        }
+        List<String> list = Arrays.asList("Murata", "Okada", "Tanimoto");
+        Stream<String> stream = list.stream();
+        stream.forEach(System.out::println);
     }
 }
