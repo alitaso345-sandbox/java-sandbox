e05e5f3310e62bed912a1789e020c155c3f5a1bf
alitaso345 <alice.maru345@gmail.com>
2019-08-04 21:02:56 +0900

    collect

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,26 +1,25 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Group> groups = new ArrayList<>();
+        List<String> list = Arrays.asList("Murata", "Okada", "Tanimoto");
 
-        Group group1 = new Group();
-        group1.add(new Student("Murata", 100));
-        group1.add(new Student("Tanimoto", 60));
-        group1.add(new Student("Okada", 80));
-        groups.add(group1);
+        List<String> newList = list.stream()
+                .filter(p -> p.length() > 5)
+                .map(p -> "[" + p + "]")
+                .collect(Collectors.toList());
 
+        newList.forEach(System.out::println);
 
-        Group group2 = new Group();
-        group2.add(new Student("Akiba", 75));
-        group2.add(new Student("Hayakawa", 85));
-        group2.add(new Student("Sakamoto", 95));
-        groups.add(group2);
+        int count = 5;
 
-        Stream<List<Student>> mappedSteam = groups.stream().map(g -> g.getStudents());
-        Stream<Student> flatMappedStream = groups.stream().flatMap(g -> g.getStudents().stream());
+        String query = IntStream.range(0, count)
+                .mapToObj(i -> "?")
+                .collect(Collectors.joining(", "));
+        System.out.println(query);
     }
 }
