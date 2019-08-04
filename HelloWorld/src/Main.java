68472327c717ac77aa48d6eda172b52e12072ee2
alitaso345 <alice.maru345@gmail.com>
2019-08-04 20:27:01 +0900

    create stream from map

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,11 +1,17 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] array = {"Murata", "Okada", "Tanimoto"};
-        Stream<String> stream = Arrays.stream(array);
-        stream.forEach(System.out::println);
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "Muata");
+        map.put("2", "Okada");
+        map.put("3", "Tanimoto");
+
+        Stream<Entry<String, String>> stream = map.entrySet().stream();
+        stream.forEach(e -> System.out.println(e.getKey() + ":" + e.getValue()));
     }
 }
